1b0936c29cfda02a9a23371105f6c89e196ca65d
lvaotian <2385933361@qq.com>
2023-09-18 20:02:49 +0800

    基础架构-小程序端Pinia持久化

--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -27,6 +27,16 @@ export const useMemberStore = defineStore(
   },
   // TODO: 持久化
   {
-    persist: true,
+    persist: {
+      // 调整为兼容多端的API
+      storage: {
+        setItem(key, value) {
+          uni.setStorageSync(key, value)
+        },
+        getItem(key) {
+          return uni.getStorageSync(key)
+        },
+      },
+    },
   },
 )
