a48d2836154e9d1daeb8b1856d4c7bc296e93b7e
lvaotian <2385933361@qq.com>
2023-09-19 15:41:49 +0800

    首页-自定义导航栏

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-//
+import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
-  <view class="index">index</view>
+  <view class="index">
+    <!-- 自定义导航栏 -->
+    <CustomNavbar />
+  </view>
 </template>
 
 <style lang="scss">
