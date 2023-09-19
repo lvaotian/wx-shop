c751d70f400d376d4f99a1e07de986f873bfeb86
lvaotian <2385933361@qq.com>
2023-09-19 17:48:41 +0800

    轮播图-获取轮播图数据、数据类型定义和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
+import type { BannerItem } from '@/types/home'
 import CustomNavbar from './components/CustomNavbar.vue'
+import { getHomeBannerAPI } from '@/services/home'
+import { ref } from 'vue'
+import { onLoad } from '@dcloudio/uni-app'
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <view class="index">
     <!-- 自定义导航栏 -->
     <CustomNavbar />
-    <XtxSwiper />
+    <!-- 自定义轮播图 -->
+    <XtxSwiper :list="bannerList" />
   </view>
 </template>
 
