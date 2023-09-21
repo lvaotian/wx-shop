7a3a8927aa018c6f72ba68e101b39eb47ff07a6d
lvaotian <2385933361@qq.com>
2023-09-21 15:27:10 +0800

    商品分类-骨架屏

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -5,6 +5,7 @@ import type { CategoryTopItem } from '@/types/category'
 import type { BannerItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { computed, ref } from 'vue'
+import PageSkeleton from './components/PageSkeleton.vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -20,12 +21,13 @@ const getCategoryTopData = async () => {
 }
 // 高亮下标
 const activeIndex = ref(0)
+// 是否数据加载完毕
+const isFinish = ref(false)
 // 页面加载
-onLoad(() => {
-  getBannerData()
-  getCategoryTopData()
+onLoad(async () => {
+  await Promise.all([getBannerData(), getCategoryTopData()])
+  isFinish.value = true
 })
-
 // 提取当前二级分类数据
 const subCategoryList = computed(() => {
   return categoryList.value[activeIndex.value]?.children || []
@@ -33,7 +35,7 @@ const subCategoryList = computed(() => {
 </script>
 
 <template>
-  <view class="viewport">
+  <view class="viewport" v-if="isFinish">
     <!-- 搜索框 -->
     <view class="search">
       <view class="input">
@@ -84,6 +86,7 @@ const subCategoryList = computed(() => {
       </scroll-view>
     </view>
   </view>
+  <PageSkeleton v-else />
 </template>
 
 <style lang="scss">
