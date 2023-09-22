459b398a72dc64b22ae53188c2c2e2e29fddc4b0
lvaotian <2385933361@qq.com>
2023-09-22 16:27:50 +0800

    登录模块-模拟快捷登录和保存登录信息

--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -1,3 +1,5 @@
+// src/stores/modules/member.ts
+import type { LoginResult } from '@/types/member'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
@@ -6,10 +8,10 @@ export const useMemberStore = defineStore(
   'member',
   () => {
     // 会员信息
-    const profile = ref<any>()
+    const profile = ref<LoginResult>()
 
     // 保存会员信息，登录时使用
-    const setProfile = (val: any) => {
+    const setProfile = (val: LoginResult) => {
       profile.value = val
     }
 
@@ -19,22 +21,17 @@ export const useMemberStore = defineStore(
     }
 
     // 记得 return
-    return {
-      profile,
-      setProfile,
-      clearProfile,
-    }
+    return { profile, setProfile, clearProfile }
   },
-  // TODO: 持久化
   {
+    // 小程序端配置
     persist: {
-      // 调整为兼容多端的API
       storage: {
-        setItem(key, value) {
-          uni.setStorageSync(key, value)
-        },
         getItem(key) {
           return uni.getStorageSync(key)
+        },
+        setItem(key, value) {
+          uni.setStorageSync(key, value)
         },
       },
     },
